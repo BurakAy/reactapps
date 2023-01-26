8dd9c43134e53010cf5fc1ce5934f09d0b3434ee
Burak <14030652+BurakAy@users.noreply.github.com>
2023-01-26 10:46:11 -0600

    added header element

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import logo from "./logo.svg";
 
 function App() {
   return (
-    <nav className="header">
-      <img src={logo} alt="logo" className="app-logo" />
-      <ul>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
-    </nav>
+    <header>
+      <nav className="header">
+        <img src={logo} alt="logo" className="app-logo" />
+        <ul>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
   );
 }
 
